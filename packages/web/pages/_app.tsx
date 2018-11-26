f173e5c0c9c7fa987f0cec087d82a693cd79c2d0
Hardys <hardyscc@gmail.com>
2018-11-26 11:23:49 +0800

    add packages/web

---
+++ packages/web/pages/_app.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+import { ApolloProvider } from 'react-apollo';
+
+import withApolloClient from '../lib/with-apollo-client';
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps, apolloClient } = this.props as any;
+    return (
+      <Container>
+        <ApolloProvider client={apolloClient}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </Container>
+    );
+  }
+}
+
+export default withApolloClient(MyApp);
---
+++ packages/web/pages/register.tsx
@@ -0,0 +1,7 @@
+import Layout from '../components/Layout';
+
+export default () => (
+  <Layout>
+    <h1>Hello Register.js 👋</h1>
+  </Layout>
+);
